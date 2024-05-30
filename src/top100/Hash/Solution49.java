78e6e2691d3d0542262a9c24407f91e90c0670a1
Zorn Wang <zornw@foxmail.com>
2024-05-30 15:45:18 +0800

    49.字母异位词分组

---
+++ src/top100/Hash/Solution49.java
@@ -0,0 +1,18 @@
+package top100.Hash;
+
+import java.util.*;
+
+public class Solution49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String key = new String(charArray);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
