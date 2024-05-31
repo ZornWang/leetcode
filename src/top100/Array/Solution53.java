8dd9069f44797cd2bf3176fd5152553efed10dad
Zorn Wang <zornw@foxmail.com>
2024-05-31 20:56:51 +0800

    53.最大子数组和

---
+++ src/top100/Array/Solution53.java
@@ -0,0 +1,19 @@
+package top100.Array;
+
+public class Solution53 {
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(new Solution53().maxSubArray(nums));
+    }
+    public int maxSubArray(int[] nums) {
+        int maxSoFar = nums[0];
+        int currentMax = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentMax = Math.max(nums[i], currentMax + nums[i]);
+            maxSoFar = Math.max(maxSoFar, currentMax);
+        }
+
+        return maxSoFar;
+    }
+}
