92e19b54e8831a203c15cc7bb97c179ca8e87823
Zorn Wang <zornw@foxmail.com>
2024-06-02 20:59:29 +0800

    48.旋转图像

---
+++ src/top100/Matrix/Solution48.java
@@ -0,0 +1,24 @@
+package top100.Matrix;
+
+import java.util.Arrays;
+
+public class Solution48 {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2}, {3, 4}};
+        new Solution48().rotate(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+    }
+
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < (n + 1) / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+}
