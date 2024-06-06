914476ff2820eef471858602973325baabe9e36e
Zorn Wang <zornw@foxmail.com>
2024-06-06 15:40:57 +0800

    142. 环形链表 II

--- src/top100/LinkedList/Solution142.java
+++ src/top100/LinkedList/Solution142.java
@@ -1,2 +1,20 @@
-package top100.LinkedList;public class Solution142 {
+package top100.LinkedList;
+
+import common.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution142 {
+    public ListNode detectCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        ListNode p = head;
+        while (p != null) {
+            if (!set.add(p)) {
+                return p;
+            }
+            p = p.next;
+        }
+        return null;
+    }
 }
