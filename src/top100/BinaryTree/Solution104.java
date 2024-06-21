281067997e2da98c0a73a00b0f322a5c1dad240e
Zorn Wang <zornw@foxmail.com>
2024-06-21 21:25:31 +0800

    104. 二叉树的最大深度

--- src/top100/BinaryTree/Solution104.java
+++ src/top100/BinaryTree/Solution104.java
@@ -4,7 +4,7 @@ import common.TreeNode;
 
 public class Solution104 {
     public static void main(String[] args) {
-        Integer[] list = new Integer[]{3, 9, 20, 0, 0, 15, 7};
+        Integer[] list = new Integer[]{3, 9, 20, null, null, 15, 7};
         TreeNode root = TreeNode.createBinaryTreeByArray(list, 0);
         System.out.println(new Solution104().maxDepth(root));
     }
