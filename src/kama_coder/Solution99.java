bb9814cfb0f333d689f750d4911d3927a9c5bfbd
Zorn Wang <zornw@foxmail.com>
2024-07-05 15:12:41 +0800

    [kama] 99. 岛屿数量

---
+++ src/kama_coder/Solution99.java
@@ -0,0 +1,77 @@
+package kama_coder;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Solution99 {
+    public static int[][] dir = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        int[][] grid = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                grid[i][j] = scanner.nextInt();
+            }
+        }
+
+        boolean[][] visited = new boolean[n][m];
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visited[i][j] && grid[i][j] == 1) {
+                    result++;
+                    // dfs
+//                    dfs(grid, visited, i, j);
+                    // bfs
+                    bfs(grid, visited, i, j);
+                }
+            }
+        }
+        System.out.print(result);
+    }
+
+    public static void dfs(int[][] grid, boolean[][] visited, int x, int y) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int n_x = x + dir[i][0];
+            int n_y = y + dir[i][1];
+            if (n_x < 0 || n_y < 0 || n_x >= grid.length || n_y >= grid[0].length) {
+                continue;
+            }
+            if (!visited[n_x][n_y] && grid[n_x][n_y] == 1) {
+                visited[n_x][n_y] = true;
+                dfs(grid, visited, n_x, n_y);
+            }
+        }
+    }
+
+    public static void bfs(int[][] grid, boolean[][] visited, int x, int y) {
+        Deque<int[]> que = new LinkedList<>();
+        que.offer(new int[]{x, y});
+        visited[x][y] = true;
+
+        while (!que.isEmpty()) {
+            int[] cur = que.poll();
+            int curx = cur[0];
+            int cury = cur[1];
+            for (int i = 0; i < 4; i++) {
+                int n_x = curx + dir[i][0];
+                int n_y = cury + dir[i][1];
+                if (n_x < 0 || n_x >= grid.length || n_y < 0 || n_y >= grid[0].length) {
+                    continue;
+                }
+                if (!visited[n_x][n_y] && grid[n_x][n_y] == 1) {
+                    que.offer(new int[]{n_x, n_y});
+                    visited[n_x][n_y] = true;
+                }
+            }
+        }
+    }
+}
